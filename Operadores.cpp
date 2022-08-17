082d0736ee2401ba0ec0e075bd94b5c53dc9c08a
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-08-17 01:31:11 -0400

    add: aula 6

---
+++ Operadores.cpp
@@ -0,0 +1,64 @@
+/*
+simbolo   	Descrição
+::	        Resolução de escopo
+++          Incremento
+--          Decremento
+()          Parênteses (chamada de função)
+[]          Elemento de arranjo
+.           Seleção de elemento por identificador
+->          Seleção de elemento por ponteiro
+
+! ~         Não lógico e complemento
+(tipo)      Conversão de tipo de dado ,casting
+*           Desreferência
+&           Referência (endereço de elemento)
+sizeof      tamanho de elemento
+new[]       Alocação dinâmica de memória
+delete[]    Desalocação dinâmica de memória
+
+.* ->*	    Ponteiro para membro
++           Adição
+-           subtração
+*           Multiplicação
+/           Divisão
+%           Módulo (resto)
+
+<<          Deslocamento de bits à esquerda
+>>          Deslocamento de bits à direita
+<           Menor que”
+<=          Menor ou igual que
+>           Maior que
+>=          Maior ou igual que
+
+==          Igual a
+!=	        Diferente de
+&	          E para bits
+^	          Ou exclusivo para bits
+|	          Ou para bits
+&&	        E lógico
+||	        Ou lógico
+x ? y : z	  Condição ternária	direita para esquerda
+=           Atribuição
++=          Atribuição por adição
+-=          Atribuição por subtração
+*=          Atribuição por multiplicação
+/=          Atribuição por divisão
+%=          Atribuição por módulo (resto)
+<<=         Atribuição por deslocalmento de bits a esquerda
+>>=         Atribuição por deslocalmento de bits a direita
+&=          Atribuição por operação lógica E
+|=          Atribuição por operação lógica OU
+^=          Atribuição por operação lógica OU exclusivo
+throw	      Lançamento de exceção
+,	          Vírgula
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+  cout << (24+5);
+
+  return(0);
+}
