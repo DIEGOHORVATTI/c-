4c88a2bc0d1068936858797f158cc5097753ada8
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-08-16 23:55:13 -0400

    add: aula 4

--- Variaveis.cpp
+++ Variaveis.cpp
@@ -5,7 +5,14 @@ int main(void){
   
   // cout << | escrever no terminal
   // cin >>  | ler do teclado
-
+  
+  cout << "Tamanho de int e:\t\t" << sizeof(int) << "bytes\n";
+  cout << "Tamanho de short int e:\t\t" << sizeof(short) << "bytes\n";
+  cout << "Tamanho de long int e:\t\t" << sizeof(long) << "bytes\n";
+  cout << "Tamanho de char e:\t\t" << sizeof(char) << "bytes\n";
+  cout << "Tamanho de bool e:\t\t" << sizeof(bool) << "bytes\n";
+  cout << "Tamanho de float e:\t\t" << sizeof(float) << "bytes\n";
+  cout << "Tamanho de double e:\t\t" << sizeof(double) << "bytes\n\n";
   int vidas = 7;           // inteiro
   char letra = 'A';        // caracter
   double decimal_1 = 2.2984894984894;  // double, numero flutuante com precisão
